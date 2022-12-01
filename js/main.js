0acb5d33e9cd148700aa213240daf8fbba0a5db5
Pernilla Lumikero <113108559+pernillalumikero@users.noreply.github.com>
2022-12-01 15:10:47 +0100

    instructions removed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-/** 
- * Kopiera in koden från föregående uppgift "6-exercise"
- * 
- * Bygg vidare på inläggslistan
- * - Se till att varje inlägg endast visar sin rubrik, resten av inlägget skall vara dolt
- * - Rubriken skall vara en klickbar länk (som EJ laddar om sidan)
- * - När man trycker på en rubrik, då skall inläggets innehåll visas under rubriken. Trycker man igen, då döljs innehållet återigen.
- * - Extra (jQuery): Försök lägga till en animation som visar/döljer inläggsinnehållet på ett snyggt sätt. Ex slide up/slide down
- * 
- *
- */
-
 //start link reloads page to get back to start
 let startLink = document.getElementById("start-link");
 
